96275ccbcbf845c2709a5d9229d519a04a4a135a
Kacper Budnik <84456252+KacperBudnik@users.noreply.github.com>
2022-04-22 12:49:17 +0200

    Update

--- Lista 7 zad 9-11.sql
+++ Lista 7 zad 9-11.sql
@@ -1,41 +1,3 @@
-# Zad 1
-Select first_name, last_name, email, address, phone from staff left join address using(address_id);
-
-# Zad 2 
-Select city, country From city left join country using (country_id);
-
-# Zad 3
-Select address, city, country from address left join (city left join country using (country_id)) using(city_id);
-
-# Zad 4
-Select first_name, last_name, email, address, phone, city, country 
-from staff left join (address left join (city left join country using (country_id)) using(city_id)) using(address_id);
-
-# Zad 5
-SELECT rental.staff_id, inventory.store_id, COUNT(*)
-FROM rental JOIN inventory USING(inventory_id)
-GROUP BY staff_id, store_id;
-
-# Zad 6
-Select first_name, last_name, GROUP_CONCAT(distinct title ORDER BY title SEPARATOR ", ") from actor left join (film left join film_actor using(film_id)) using(actor_id)
-GROUP BY first_name, last_name ;
-
-# Zad 7
-Select first_name, last_name,GROUP_CONCAT(distinct title ORDER BY title SEPARATOR ", ") from actor left join (film left join film_actor using(film_id)) using(actor_id)
-WHERE title Like "%EGG%"
-Group By first_name, last_name;
-
-# Zad 8
-Select customer.first_name, customer.last_name, customer.email, address.address, address.phone, city.city, country.country_id, staff.first_name, staff.last_name
-FROM (customer 
-        left JOIN (address 
-            left JOIN (city 
-                left JOIN country using(country_id))
-            using(city_id))
-        using(address_id))
-        left JOIN staff using(store_id)
-WHERE (customer.first_name, customer.last_name)=("ALAN","KAHN");
-
 # Zad 9
 Select customer.first_name, customer.last_name, title, rental_date, return_date, amount, payment_date
 from  customer 
@@ -85,5 +47,3 @@ From address
     left join store using(address_id)
 Where customer.first_name is NULL and staff.first_name is NULL and store.store_id is NULL
 # nie ma takich
-
-
